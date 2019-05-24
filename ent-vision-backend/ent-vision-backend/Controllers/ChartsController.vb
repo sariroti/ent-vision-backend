d73ef41be505c734e9ac7383e52ab3d531fc372a
sariroti <airen142004@yahoo.com>
2019-05-24 08:37:51 +0700

    Chart API
    - Add GET product total by categories

---
+++ ent-vision-backend/ent-vision-backend/Controllers/ChartsController.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Net
+Imports System.Web.Http
+
+Namespace Controllers
+    Public Class ChartsController
+        Inherits ApiController
+
+        <Route("api/charts/GetProductTotalByCategories")>
+        Public Function GetProductTotalByCategories() As IEnumerable(Of Model.GoogleLineChart)
+            Using ctx As EntVisionContext = New EntVisionContext()
+                Dim query = (From p In ctx.Product
+                             Group p By p.CategoryName Into CategoryGroup = Group
+                             Select CategoryName, CategoryGroup.Count()).ToList()
+
+                Dim googleLineChartList As List(Of Model.GoogleLineChart) = New List(Of Model.GoogleLineChart)()
+
+                For Each q In query
+                    Dim googleLineChart As Model.GoogleLineChart = New Model.GoogleLineChart()
+                    googleLineChart.Rows = New List(Of String)
+                    googleLineChart.Rows.Add(q.CategoryName)
+                    googleLineChart.Rows.Add(q.Count)
+
+                    googleLineChartList.Add(googleLineChart)
+                Next
+
+                Return googleLineChartList
+
+            End Using
+        End Function
+
+    End Class
+End Namespace
